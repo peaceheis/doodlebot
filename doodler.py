9f0068914d45beba01918bcef1853e6556ac6f04
peaceheis <69596656+peaceheis@users.noreply.github.com>
2021-03-16 20:04:00 -0500

    Add files via upload

--- doodler.py
+++ doodler.py
@@ -23,12 +23,30 @@ async def prompts(ctx) :
     global promptlist
     if len(promptlist) == 0 : 
         await ctx.send("None!")
+        return
+    response = "**Prompts**: \n" + str(promptlist).replace('[', '').replace(']', '')
+    if len(response) <= 1980 :
+        await ctx.send(response) 
     else : 
-        response = "0: \"" + promptlist[0] + "\". "
-        for i in range(len(promptlist) - 1):
-            response += str(i + 1) + ": " 
-            response += promptlist[i+1] + " "
-        await ctx.send(response)
+        response_list = promptlist
+        current_char = 0 
+        count = 0
+        count1 = 0 
+        should_continue = True
+        while should_continue :
+            current_response = ""         
+            for i in range(25) :
+                try :
+                    current_response += str(count1) + ": "                    
+                    current_response += response_list[count1].replace('\'', "")
+                    current_response += " "
+                    count1 += 1
+                except : 
+                    await ctx.send(current_response)
+                    return
+            count += 1
+            current_response = current_response + "(" + str(count) + ")"
+            await ctx.send(current_response)
 
 @bot.command(name = 'remove')
 async def remove(ctx, arg) : 
@@ -45,31 +63,74 @@ async def remove(ctx, arg) :
 async def load_prompts(ctx, *args) : 
     global promptlist
     for arg in args: 
-        promptlist.append(arg)
+        promptlist.append(arg)    
     response = "Updated promptlist to " + str(promptlist) + "!"
-    await ctx.send(response)
+    if len(response) <= 2000 :
+        await ctx.send(response)
+    else : 
+        response = "I can't show you the whole list, but you can rest assured everything went well. Feel free to use ?prompts! :wink:"
+        await ctx.send(response)
+        
+@bot.command(name = 'num_prompts')
+async def num_prompts(ctx) :
+    global promptlist
+    await ctx.send("There are " + str(len(promptlist)) + " prompts!")
+                   
 
-def remove_item(arg) : 
-    global promptlist 
-    promptlist = promptlist.pop(arg)
-    return promptlist 
 
 @bot.command(name = 'commence')
 async def commence(ctx): 
-    global promptlist 
+    global promptlist
+    channel = bot.get_channel(818546231868391454)
+    await back_up(ctx)
     while True :
-        good_times = [0,3,6]
-        if time.gmtime().tm_hour == 11 and time.gmtime().tm_min <= 29 and time.gmtime().tm_hr /2:
+        good_times = [0, 2, 4]
+        if time.gmtime().tm_hour == 11 and time.gmtime().tm_min <= 29 and len(promptlist) > 0 :
             random_num = random.randint(0, len(promptlist) - 1)
             channel = bot.get_channel(816135387339685930)
-            await channel.send(promptlist[random_num])
+            await channel.send(promptlist[random_num]) 
             del promptlist[random_num]
             time.sleep(1800)
-        if time.gmtime().tm_day in good_times and time.gmtime().tm_hour == 1 and time.gmtime().min == 0 and time.gmtime.sec == 0 : 
+        if time.gmtime().tm_wday in good_times and time.gmtime().tm_hour == 1 and time.gmtime().tm_min <= 10 : 
             channel = bot.get_channel(818546231868391454)
-            await channel.send(str(promptlist).replace('[', '').replace(']', '').replace(',', '')) 
-            time.sleep(60)
+            await back_up(ctx)
+            time.sleep(600)
+            
+
+@bot.command(name = 'back_up') 
+async def back_up(ctx) : 
+    channel = bot.get_channel(818546231868391454)
+    global promptlist
+    if len(promptlist) == 0 : 
+        await channel.send("Nothing to back up!")
+        return
+    response = "**Backup**: \n" + str(promptlist).replace('[', '').replace(']', '')
+    if len(response) <= 1980 :
+        await channel.send(response) 
+    else : 
+        response_list = promptlist
+        current_char = 0 
+        count = 0
+        count1 = 0 
+        should_continue = True
+        while should_continue :
+            current_response = ""         
+            for i in range(25) :
+                try :
+                    current_response += '\"'
+                    current_response += response_list[count1].replace('\'', "")
+                    current_response += '\" '
+                    count1 += 1
+                except : 
+                    await channel.send(current_response)
+                    return
+            count += 1
+            current_response = current_response + "(" + str(count) + ")"
+            await channel.send(current_response)           
+            
+ 
+ 
 
     
 bot.run(TOKEN)
-print("something")
+
